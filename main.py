26a04d123bf994d83ad87bbd459846e417fd3fa8
Uday Teja k <uteja70@gmail.com>
2022-07-21 00:22:55 +0530

    adding crud

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-from typing import Optional
-from fastapi import Body, FastAPI
-from pydantic import BaseModel, StrictStr
+from random import randrange
+from urllib import response
+from fastapi import FastAPI, HTTPException, Response, status
+from pydantic import StrictInt
+from models.post import Post
 
 app = FastAPI()
+
+my_posts = [
+    {
+        "id": 1,
+        "title": "beaches in florida",
+        "content": "check out these beaches",
+    },
+    {
+        "id": 2,
+        "title": "pizza in florida",
+        "content": "check out these pizza",
+    },
+]
 
 
 @app.get("/")
@@ -10,19 +25,33 @@ async def getHello():
     return {"messege": "Hello World"}
 
 
-@app.get("/getposts")
+@app.get("/posts")
 def get_posts():
-    return {"data": "This is your posts"}
+    return my_posts
 
 
-class Post(BaseModel):
-    title: StrictStr
-    content: StrictStr
-    published: bool = True
-    rating: Optional[int] = None
+def find_post(id):
+    for post in my_posts:
+        if post["id"] == id:
+            return post
 
 
-@app.post("/createpost")
+@app.get("/post/{id}")
+def get_post(id: int, response: Response):
+
+    post = find_post(id)
+    if not post:
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return f"{id} Not Found"
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"{id} not found"
+        )
+    return post
+
+
+@app.post("/createpost", status_code=status.HTTP_201_CREATED)
 def create_post(new_post: Post):
-    print(new_post.title)
-    return new_post
+    new_post_dict = new_post.dict()
+    new_post_dict["id"] = randrange(0, 5000)
+    my_posts.append(new_post_dict)
+    return new_post_dict
---
+++ models/post.py
@@ -0,0 +1,9 @@
+from typing import Optional
+from pydantic import BaseModel, StrictStr
+
+
+class Post(BaseModel):
+    title: StrictStr
+    content: StrictStr
+    published: bool = True
+    rating: Optional[int] = None
