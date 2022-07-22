babdf4c9e6f3c161e3c3c1e9457530edcde27018
Uday Teja k <uteja70@gmail.com>
2022-07-22 11:22:44 +0530

    refactoring project

--- app/main.py
+++ app/main.py
@@ -1,7 +1,5 @@
 from random import randrange
-from urllib import response
 from fastapi import FastAPI, HTTPException, Response, status
-from pydantic import StrictInt
 from models.post import Post
 
 app = FastAPI()
@@ -36,6 +34,12 @@ def find_post(id):
             return post
 
 
+def find_index_post(id):
+    for i, post in enumerate(my_posts):
+        if post["id"] == id:
+            return i
+
+
 @app.get("/post/{id}")
 def get_post(id: int, response: Response):
 
@@ -55,3 +59,27 @@ def create_post(new_post: Post):
     new_post_dict["id"] = randrange(0, 5000)
     my_posts.append(new_post_dict)
     return new_post_dict
+
+
+@app.delete("/post/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"{id} not found"
+        )
+    my_posts.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/post/{id}")
+def update_post(id: int, post: Post):
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"{id} not found"
+        )
+    post_dict = post.dict()
+    post_dict["id"] = id
+    my_posts[index] = post_dict
+    return post_dict
